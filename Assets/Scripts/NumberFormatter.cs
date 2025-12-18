2f72ccdd6758e2e92b72f8fc8ce15fdeb4cd5234
yamazaki <ymzkkrsk1224@gmail.com>
2025-12-18 14:49:18 +0900

    円の表示を追加

--- Assets/Scripts/NumberFormatter.cs
+++ Assets/Scripts/NumberFormatter.cs
@@ -4,22 +4,22 @@ public static class NumberFormatter
     {
         if (value < 1000)
         {
-            return value.ToString();
+            return value.ToString() + "‰~";
         }
         else if (value < 10_000)
         {
             // 1ç ` 9.9ç
-            return (value / 1000f).ToString("0.#") + "ç";
+            return (value / 1000f).ToString("0.#") + "ç‰~";
         }
         else if (value < 100_000_000)
         {
             // 1–œ ` 9999–œ
-            return (value / 10_000f).ToString("0.#") + "–œ";
+            return (value / 10_000f).ToString("0.#") + "–œ‰~";
         }
         else
         {
             // 1‰­ˆÈã
-            return (value / 100_000_000f).ToString("0.#") + "‰­";
+            return (value / 100_000_000f).ToString("0.#") + "‰­‰~";
         }
     }
 }

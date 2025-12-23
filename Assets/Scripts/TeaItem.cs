e2f561f0a90ce9576bd643f51ede1c7bd8afdeb1
yamazaki <ymzkkrsk1224@gmail.com>
2025-12-23 13:40:12 +0900

    「購入状態」を追加

--- Assets/Scripts/TeaItem.cs
+++ Assets/Scripts/TeaItem.cs
@@ -1,21 +1,40 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class TeaItem : MonoBehaviour
 {
     [Header("お茶の基本データ")]
     public int basePrice = 100;
 
+    [Header("購入状態")]
+    public bool isUnlocked = false;
+
     [Header("UI")]
     public TMP_Text priceText;
+    public Image itemImage;
 
     void Start()
     {
-        UpdatePriceText();
+        UpdateView();
     }
 
-    public void UpdatePriceText()
+    void UpdateView()
+    {
+        UpdatePriceText();
+        UpdateVisual();
+    }
+
+    void UpdatePriceText()
     {
         priceText.text = NumberFormatter.Format(basePrice);
+    }
+
+    void UpdateVisual()
+    {
+        if (itemImage == null) return;
+
+        // 未購入ならグレーアウト
+        itemImage.color = isUnlocked ? Color.white : Color.gray;
     }
 }
